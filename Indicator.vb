42d799a86612447e5196af9a4932b2c6f09119ef
D*Squirryl <35942162+davuni@users.noreply.github.com>
2020-05-13 20:37:42 +0800

    Add files via upload

---
+++ Indicator.vb
@@ -0,0 +1,86 @@
+Sub Indicator()
+' bydukenuke@newsmth.net
+' 2010/7/11 06:13
+'
+' Update by oicu#lsxk.org
+' 2010/9/12 20:44
+'
+' Arc shape ported by D. Squirryl
+' 2020/5/13 20:32
+
+    Dim mySlides As Slides
+    Dim pageBar As ShapeRange
+    Dim pageIndicator As Shape
+    Dim pageWidth, pageHeight, pageStep
+    Dim MyArray() As Variant
+    Dim i, j, k
+    j = 0
+    k = 0
+
+    Set mySlides = Application.ActivePresentation.Slides
+    pageWidth = Application.ActivePresentation.SlideMaster.Width
+    pageHeight = Application.ActivePresentation.SlideMaster.Height
+    
+' Parameters
+    Dim x, y, w, h, r, color, colorbg
+    x = pageWidth - 90
+    y = 40
+    w = 50
+    h = 50
+    r = 0.1
+    color = RGB(190, 213, 247)
+    colorbg = RGB(95, 95, 95)
+    colorfocus = RGB(127, 156, 212)
+' Parameters
+
+    ReDim MyArray(mySlides.Count, 0)
+    For i = 1 To mySlides.Count
+        If mySlides.Item(i).SlideShowTransition.Hidden = True Then
+            j = j + 1
+            MyArray(i, 0) = 1
+        Else
+            MyArray(i, 0) = 0
+        End If
+    Next
+    If mySlides.Count - j > 0 Then
+        pageStep = 360 / (mySlides.Count - j)
+    Else
+        pageStep = 0
+    End If
+    On Error Resume Next
+    For i = 1 To mySlides.Count
+        k = k + MyArray(i, 0)
+        Set pageBar = mySlides.Item(i).Shapes.Range(Array())
+        Set pageBar = mySlides.Item(i).Shapes.Range(Array("RectanglePageNum"))
+        If IsNull(pageBar) Or pageBar.Count = 0 Then GoTo newIndicator
+        Set pageIndicator = pageBar.Item(1)
+        GoTo nextPage
+        
+newIndicator:
+        Set pageIndicatorBG = mySlides.Item(i).Shapes.AddShape(msoShapeBlockArc, x, y, w, h)
+        Set pageIndicator = mySlides.Item(i).Shapes.AddShape(msoShapeBlockArc, x, y, w, h)
+        Set pageIndicatorFocus = mySlides.Item(i).Shapes.AddShape(msoShapeBlockArc, x, y, w, h)
+        pageIndicator.Fill.ForeColor.RGB = color
+        pageIndicatorBG.Fill.ForeColor.RGB = colorbg
+        pageIndicatorFocus.Fill.ForeColor.RGB = colorfocus
+        pageIndicator.Line.Visible = msoFalse
+        pageIndicatorBG.Line.Visible = msoFalse
+        pageIndicatorFocus.Line.Visible = msoFalse
+        pageIndicator.Adjustments.Item(1) = -90
+        pageIndicator.Adjustments.Item(2) = pageStep - 90
+        pageIndicator.Adjustments.Item(3) = r
+        pageIndicatorBG.Adjustments.Item(1) = 0
+        pageIndicatorBG.Adjustments.Item(2) = 360
+        pageIndicatorBG.Adjustments.Item(3) = r
+        pageIndicatorFocus.Adjustments.Item(1) = -90
+        pageIndicatorFocus.Adjustments.Item(2) = pageStep - 90
+        pageIndicatorFocus.Adjustments.Item(3) = r
+
+nextPage:
+        pageIndicator.Adjustments.Item(2) = i * pageStep - 90
+        pageIndicatorFocus.Adjustments.Item(1) = (i - 1) * pageStep - 90
+        pageIndicatorFocus.Adjustments.Item(2) = i * pageStep - 90
+        
+    Next
+End Sub
+
